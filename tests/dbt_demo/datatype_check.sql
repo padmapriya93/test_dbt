c8f405f7125ee3f55603e641bd6bc860b684ad9f
Padmapriya R <rpadmapriya@altimetrik.com>
2023-11-17 12:52:38 +0530

    added new test

--- tests/dbt_demo/datatype_check.sql
+++ tests/dbt_demo/datatype_check.sql
@@ -1,11 +1,10 @@
 
-
+with 
 select
   'Data Type Validation' as test,
   case
     when
-      all(email::text ~ '^[0-9]+$' and hire_date::date is not null)
-    then ''
-    else 'FAIL'
+      all(email::text ~ '[0-9]+$' and hire_date::date is null)
+    then 'FAIL'
   end as status
 from {{ref('emp')}}
