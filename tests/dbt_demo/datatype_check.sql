96843f38d55a7c22ab84d4316a8b7a033d9924ac
Padmapriya R <rpadmapriya@altimetrik.com>
2023-11-17 12:59:48 +0530

    ade

--- tests/dbt_demo/datatype_check.sql
+++ tests/dbt_demo/datatype_check.sql
@@ -3,11 +3,10 @@ select
   'Data Type Validation' as test,
   case
     when
-      all(email::text ~ '^[0-9]+$' or hire_date::date is not null)
+      hire_date is not null
     then 'PASS'
 	else 'FAIL'
   end as status
 from {{ref('emp')}}
 )
-
 select * from data_type where status <>'PASS'
