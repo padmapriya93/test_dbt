d19aca324093644f23cd59033ccc52d7fe61e945
Padmapriya R <rpadmapriya@altimetrik.com>
2023-11-17 12:49:18 +0530

    added new trst

--- tests/dbt_demo/datatype_check.sql
+++ tests/dbt_demo/datatype_check.sql
@@ -1,9 +1,11 @@
+
+
 select
   'Data Type Validation' as test,
   case
     when
       all(email::text ~ '^[0-9]+$' and hire_date::date is not null)
-    then 'PASS'
+    then ''
     else 'FAIL'
   end as status
 from {{ref('emp')}}
